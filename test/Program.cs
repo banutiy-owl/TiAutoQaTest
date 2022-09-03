0d66ca33b3d275784d22461f00b08935b75a35ad
Kate Kononova <01173946@pw.edu.pl>
2022-09-04 00:57:44 +0200

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter coordinate x of A dot: ");
+            double xDotA=Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter coordinate y of A dot: ");
+            double yDotA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter coordinate x of B dot: ");
+            double xDotB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter coordinate y of B dot: ");
+            double yDotB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter coordinate x of C dot: ");
+            double xDotC = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter coordinate y of C dot: ");
+            double yDotC = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Triangle triangle = new Triangle(xDotA, yDotA, xDotB, yDotB, xDotC, yDotC);
+
+            Console.WriteLine("Length of AB is: " + triangle.sideAB);
+            Console.WriteLine("Length of BC is: " + triangle.sideBC);
+            Console.WriteLine("Length of AC is: " + triangle.sideAC);
+
+            Console.WriteLine();
+
+            triangle.equiTriangle();
+            triangle.isosTriangle();
+            triangle.rightTriangle();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Perimeter: " + triangle.getPerimeter());
+
+            Console.WriteLine();
+
+            triangle.getEvenNumbers();
+        }
+    }
+}
---
+++ test/Triangle.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Triangle
+    {
+        public double xDotA;
+        public double yDotA;
+        public double xDotB;
+        public double yDotB;
+        public double xDotC;
+        public double yDotC;
+
+        public double sideAB;
+        public double sideBC;
+        public double sideAC;
+
+        public double perimeter;
+
+
+
+        public Triangle(double xDotA, double yDotA, double xDotB, double yDotB, double xDotC, double yDotC)
+        {
+            this.xDotA = xDotA;
+            this.yDotA = yDotA;
+            this.xDotB = xDotB;
+            this.yDotB = yDotB;
+            this.xDotC = xDotC;
+            this.yDotC = yDotC;
+
+            sideAB = Math.Sqrt(Math.Pow(xDotB - xDotA, 2) + Math.Pow(yDotB - yDotA, 2));
+            sideBC = Math.Sqrt(Math.Pow(xDotC - xDotB, 2) + Math.Pow(yDotC - yDotB, 2));
+            sideAC = Math.Sqrt(Math.Pow(xDotC - xDotA, 2) + Math.Pow(yDotC - yDotA, 2));
+
+            perimeter = sideAB + sideAC + sideBC;
+        }
+
+        public void equiTriangle()
+        {
+
+            if (sideAB == sideAC && sideAB == sideBC)
+            {
+                Console.WriteLine("Triangle IS Equilateral");
+            } else
+            {
+                Console.WriteLine("Triangle IS NOT Equilateral");
+            }
+        }
+
+        public void isosTriangle()
+        {
+            if (sideAB == sideBC || sideAB == sideAC || sideBC == sideAC)
+            {
+                Console.WriteLine("Triangle IS Isosceles");
+            } else
+            {
+                Console.WriteLine("Triangle IS NOT Isosceles");
+            }
+
+        }
+
+        public void rightTriangle()
+        {
+            double c2=0;
+            double a2=0;
+            double b2=0;
+
+            if (sideAB>sideAC && sideAB>sideBC)
+            {
+                c2 = Math.Pow(sideAB, 2);
+                a2 = Math.Pow(sideAC, 2);
+                b2 = Math.Pow(sideBC, 2);
+            } else if (sideAC>sideAB && sideAC>sideBC)
+            {
+                c2 = Math.Pow(sideAC, 2);
+                a2 = Math.Pow(sideAB, 2);
+                b2 = Math.Pow(sideBC, 2);
+            } else if (sideBC>sideAB && sideBC>sideAC)
+            {
+                c2 = Math.Pow(sideBC, 2);
+                a2 = Math.Pow(sideAB, 2);
+                b2 = Math.Pow(sideAC, 2);
+            } 
+
+            if ((c2-(b2+a2))<= Math.Pow(10,-6))
+            {
+                Console.WriteLine("Triangle IS Right");
+            } else
+            {
+                Console.WriteLine("Triangle IS NOT Right");
+            }
+        }
+
+        public double getPerimeter()
+        {
+            return perimeter;
+        }
+
+        public void getEvenNumbers()
+        {
+
+            Console.WriteLine("Parity numbers in range from 0 to triangle perimeter: ");
+
+            for (int i=0;i<=perimeter;i+=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
